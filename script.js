8e48cea5529e655e8581209d9a794200c65cbcac
danutibe07 <danielutibe07@gmail.com>
2023-02-07 09:37:36 +0100

    Script.jS : Selected Elements properly

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 'use strict';
 
-const menuBtn = document.querySelector('#hamburger-menu');
+const menuBtn = document.getElementById('hamburger-menu');
 const exitMenuBtn = document.querySelector('.nav-exit-button');
-const headerUl = document.querySelector('#header-ul');
+const headerUl = document.getElementById('header-ul');
 
 const portfolioLink = document.querySelector('.portfolio-link');
 const aboutLink = document.querySelector('.about-link');
 const contactLink = document.querySelector('.contact-link');
 
-const portfolioSection = document.querySelector('#section-2');
-const aboutSection = document.querySelector('#section-3');
-const contactSection = document.querySelector('#portfolio-footer');
+const portfolioSection = document.getElementById('section-2');
+const aboutSection = document.getElementById('section-3');
+const contactSection = document.getElementById('portfolio-footer');
 
 const displayNav = function() {
   headerUl.classList.add('header-ul-active');
